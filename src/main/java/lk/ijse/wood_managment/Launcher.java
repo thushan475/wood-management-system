c85f26f1cb632692547af3552a7c50962b5bfd83
thushan475 <thushanpahasara04@gmail.com>
2026-03-09 14:38:58 +0530

    first commit

--- src/main/java/lk/ijse/wood_managment/Launcher.java
+++
@@ -1,9 +0,0 @@
-package lk.ijse.wood_managment;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
--- src/main/java/lk/ijse/wood_managment/Model/Supplier.java
+++
@@ -1,4 +0,0 @@
-package lk.ijse.wood_managment.Model;
-
-public class Supplier {
-}
